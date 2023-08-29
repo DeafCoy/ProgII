c01a3727d7fbd27976bec304c5e487bd9b8018db
DeafCoy <111319875+DeafCoy@users.noreply.github.com>
2023-08-29 16:39:22 -0300

    Add files via upload

---
+++ ProyectoCarreras/Entidades/Carrera.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCarreras.Entidades
+{
+    internal class Carrera
+    {
+        public int IdCarrera { get; set; }
+        public string TituloCarrera { get; set; }
+        public List<DetalleCarrera> Detalles { get; set; }
+
+        public Carrera()
+        {
+            Detalles = new List<DetalleCarrera>();
+        }
+
+        public void AgregarDetalle(DetalleCarrera detalle)
+        {
+            Detalles.Add(detalle);
+        }
+        public void QuitarDetalles(int posicion)
+        {
+            Detalles.RemoveAt(posicion);
+        }
+
+        
+    }
+}
